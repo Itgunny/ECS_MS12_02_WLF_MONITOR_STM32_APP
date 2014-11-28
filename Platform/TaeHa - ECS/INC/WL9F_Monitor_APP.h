f284146a04bad0d08f0936ba6065f9368137ee22
fort22 <fort22@8c143bec-4d7d-2e4e-a212-1cf3cf7e8ea1>
2014-11-28 05:38:42 +0000

    git-svn-id: svn://192.168.30.90:4000/%5BSRC%5D/%5BSVN%5D%20ECS-RD12-16%20-%20WLF%20Monitor%20-%20STM32_APP@63 8c143bec-4d7d-2e4e-a212-1cf3cf7e8ea1

--- Platform/TaeHa - ECS/INC/WL9F_Monitor_APP.h
+++ Platform/TaeHa - ECS/INC/WL9F_Monitor_APP.h
@@ -76,9 +76,9 @@
 //	RevD 부터 이런 기능을 추가..
 //	TW2835, TW8816 호환하기 위하여 수정. 
 //#define BoardVersion_RevA
-#define BoardVersion_RevB
+//#define BoardVersion_RevB
 //#define BoardVersion_RevC
-//#define BoardVersion_RevD
+#define BoardVersion_RevD
 //	--, kutelf, 140801
 
 //#define     STM32_BUZZER      //  STM32 BUZZER
